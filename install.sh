ada267cceac83b7aadd3549c4e2c65c9cd97acd2
KuoE0 <kuoe0.tw@gmail.com>
2012-11-09 04:08:05 +0800

    change env path and modify depth argument to run on linux

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#! env bash
+#! /usr/bin/env bash
 
 if [ ! -d ~/bin ]; then
 	mkdir -p ~/bin
 fi
 
-for file in `find . -d 1`; do
+for file in `find . -maxdepth 1`; do
 	# get all executable file
 	if [ -x $file ] && [ ! -d $file ] && [ `basename $file` != "install.sh" ]; then
 		cp $file ~/bin/
