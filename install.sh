bfe694759577ad3bef71714f79dafecee6987154
KuoE0 <kuoe0.tw@gmail.com>
2012-11-09 14:52:45 +0800

    use link instead of copy

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env bash
 
+x_realpath() {
+	cd `dirname $1`
+	echo "$(pwd)/$(basename $1)"
+}
+
 if [ ! -d ~/bin ]; then
 	mkdir -p ~/bin
 fi
@@ -7,8 +12,15 @@ fi
 for file in `find . -maxdepth 1`; do
 	# get all executable file
 	if [ -x $file ] && [ ! -d $file ] && [ `basename $file` != "install.sh" ]; then
-		cp $file ~/bin/
+		source_file=`x_realpath $file`
+		target_file="$HOME/bin/$(basename $file)"
+
+		# remove origin file
+		if [ -e $target_file ]; then
+			rm $target_file
+		fi
+		# link to ~/bin/
+		ln -s $source_file $target_file
 	fi
 done
-	
 
